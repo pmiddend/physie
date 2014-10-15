eb54d44e6f64f0c5bfd028f80526caa26b3f37c1
Philipp Middendorf <pmi@hacon.de>
2014-10-15 14:54:41 +0200

    Minor changes to sat

--- src/Physie/Main.hs
+++ src/Physie/Main.hs
@@ -15,7 +15,7 @@ import Linear.Metric(dot,signorm,quadrance)
 import Control.Lens.TH(makeLenses)
 import Control.Monad(msum)
 import Control.Applicative((<$>),(<*>))
-import Data.Maybe(fromJust)
+import Data.Maybe(fromJust,isNothing,maybeToList)
 import Data.List(minimumBy)
 import Data.Function(on)
 
@@ -99,24 +99,23 @@ minmax :: Ord a => [a] -> (a,a)
 minmax ls = (minimum ls,maximum ls)
 
 -- Courtesy of http://elancev.name/oliver/2D%20polygon.htm
-data AxisSeparation = Separates | Overlaps (V2 Float) deriving Eq
-
+--data AxisSeparation = Separates | Overlaps (V2 Float) deriving Eq
 satIntersects :: [Line] -> [Line] -> Maybe (V2 Float)
 satIntersects a b = let axes = concatMap (map (perp . lineToVector)) [a,b]
                         separationData = map (separates a b) axes
-                    in if any (== Separates) separationData
+                    in if any isNothing separationData
                        then Nothing
-                       else Just $ minimumBy (compare `on` quadrance) $ concatMap (\x -> case x of Separates -> []; Overlaps x -> [x]) separationData
+                       else Just $ minimumBy (compare `on` quadrance) $ concatMap maybeToList separationData
   where lineToVector (Line x y) = y - x
         calculateInterval axis = minmax . map ((axis `dot`) . lineToVector)
         separates x y axis = let (mina,maxa) = calculateInterval axis x
                                  (minb,maxb) = calculateInterval axis y
                              in if mina > maxb || minb > maxa
-                                then Separates
+                                then Nothing
                                 else let d0 = maxa - minb
                                          d1 = maxb - mina
                                          depth = if d0 < d1 then d0 else d1
-                                     in Overlaps $ axis ^* (depth / (quadrance axis))
+                                     in Just $ axis ^* (depth / (quadrance axis))
 
 mainLoop :: SDLT.Renderer -> Float -> IO ()
 mainLoop renderer angle = do
